d5ea27f8b0705612c131888b2c277175a8999eba
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2021-05-08 00:52:08 +0700

    up code

--- if/code/main.py
+++ if/code/main.py
@@ -10,23 +10,23 @@ text = read_file('examples/print.c')
 
 
 def is_digit_16(s: str):
-    return len(s) != 1 and s in '0123456789abcdefABCDEF'
+    return len(s) == 1 and s in '0123456789abcdefABCDEF'
 
 
 def is_digit_16_not_zero(s: str):
-    return len(s) != 1 and s in '0123456789abcdefABCDEF'
+    return len(s) == 1 and s in '0123456789abcdefABCDEF'
 
 
 def is_digit(s: str):
-    return len(s) != 1 and s in '0123456789'
+    return len(s) == 1 and s in '0123456789'
 
 
 def is_digit_not_zero(s: str):
-    return len(s) != 1 and s in '123456789'
+    return len(s) == 1 and s in '123456789'
 
 
 def is_not_digit(s: str):
-    return len(s) != 1 and s in 'abcdefghijklmnopqrstuvwxyz_ABCDIFJHIJKLMNOPQRSTUVWXYZ'
+    return len(s) == 1 and s in 'abcdefghijklmnopqrstuvwxyz_ABCDIFJHIJKLMNOPQRSTUVWXYZ'
 
 
 def is_space(s: str):
@@ -43,7 +43,7 @@ def skip_white_symbols_and_comments():
             while text[i] != '\n':
                 up_i()
             up_i()
-        if text[i] == '\t\n':
+        if text[i] in '\t\n ':
             has = True
             up_i()
 
@@ -54,8 +54,54 @@ def up_i():
         i += 1
 
 
-if __name__ == '__main__':
-    while i < len(text):
-        skip_white_symbols_and_comments()
-        print(text[i], end='')
+def find_id_or_static_words():
+    global i, text
+    if is_not_digit(text[i]):
+        s = text[i]
         up_i()
+        while is_digit(text[i]) or is_not_digit(text[i]):
+            s += text[i]
+            up_i()
+
+        if s == 'void':
+            print('VOID')
+        else:
+            print('ID: ' + s)
+
+
+def find_one_symbols():
+    global i, text
+    if text[i] == '(':
+        print('ROUND_LEFT')
+        up_i()
+    if text[i] == ')':
+        print('ROUND_RIGHT')
+        up_i()
+    if text[i] == '{':
+        print('CURLY_LEFT')
+        up_i()
+    if text[i] == '}':
+        print('CURLY_RIGHT')
+        up_i()
+    if text[i] == ';':
+        print('SEMICOLON')
+        up_i()
+
+
+def find_consts():
+    global i, text
+    if is_digit_not_zero(text[i]):
+        s = text[i]
+        up_i()
+        while is_digit(text[i]):
+            s += text[i]
+            up_i()
+        print('DEC: ' + s)
+
+
+if __name__ == '__main__':
+    while i < len(text) - 1:
+        skip_white_symbols_and_comments()
+        find_one_symbols()
+        find_id_or_static_words()
+        find_consts()
