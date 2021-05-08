a38ee1019ebcd21827519f120b75df6b0ba53651
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2021-05-08 12:48:57 +0700

    add new files code

--- if/code/main.py
+++ if/code/main.py
@@ -1,3 +1,7 @@
+from consts import static_words, one_symbols, two_symbols
+from is_functions import is_not_digit, is_digit, is_digit_not_zero, is_digit_16_not_zero, is_digit_16
+
+
 def read_file(path: str):
     f = open(path, 'r')
     t = ''.join(f.readlines())
@@ -9,24 +13,10 @@ i = 0
 text = read_file('examples/math.c')
 
 
-def is_digit_16(s: str):
-    return len(s) == 1 and s in '0123456789abcdefABCDEF'
-
-
-def is_digit_16_not_zero(s: str):
-    return len(s) == 1 and s in '0123456789abcdefABCDEF'
-
-
-def is_digit(s: str):
-    return len(s) == 1 and s in '0123456789'
-
-
-def is_digit_not_zero(s: str):
-    return len(s) == 1 and s in '123456789'
-
-
-def is_not_digit(s: str):
-    return len(s) == 1 and s in 'abcdefghijklmnopqrstuvwxyz_ABCDIFJHIJKLMNOPQRSTUVWXYZ'
+def up_i():
+    global i
+    if i < len(text) - 1:
+        i += 1
 
 
 def skip_white_symbols_and_comments():
@@ -44,12 +34,6 @@ def skip_white_symbols_and_comments():
             up_i()
 
 
-def up_i():
-    global i
-    if i < len(text) - 1:
-        i += 1
-
-
 def find_id_or_static_words():
     global i, text
     if is_not_digit(text[i]):
@@ -59,65 +43,16 @@ def find_id_or_static_words():
             s += text[i]
             up_i()
 
-        if s == 'void':
-            print('VOID')
-        elif s == 'if':
-            print('IF')
-        elif s == 'else':
-            print('ELSE')
-        elif s == 'short':
-            print('SHORT')
-        elif s == 'int':
-            print('INT')
-        elif s == 'long':
-            print('LONG')
+        if s in static_words.keys():
+            print(static_words[s])
         else:
             print('ID: ' + s)
 
 
 def find_one_symbols():
     global i, text
-    if text[i] == '(':
-        print('ROUND_LEFT')
-        up_i()
-    if text[i] == ')':
-        print('ROUND_RIGHT')
-        up_i()
-    if text[i] == '{':
-        print('CURLY_LEFT')
-        up_i()
-    if text[i] == '}':
-        print('CURLY_RIGHT')
-        up_i()
-    if text[i] == ';':
-        print('SEMICOLON')
-        up_i()
-    if text[i] == '=':
-        print('ASSIGN')
-        up_i()
-    if text[i] == '+':
-        print('PLUS')
-        up_i()
-    if text[i] == '-':
-        print('MINUS')
-        up_i()
-    if text[i] == '*':
-        print('STAR')
-        up_i()
-    if text[i] == '/':
-        print('SLASH')
-        up_i()
-    if text[i] == '%':
-        print('PERCENT')
-        up_i()
-    if text[i] == ',':
-        print('COMMA')
-        up_i()
-    if text[i] == '<':
-        print('LESS')
-        up_i()
-    if text[i] == '>':
-        print('GREATER')
+    if text[i] in one_symbols.keys():
+        print(one_symbols[text[i]])
         up_i()
 
 
@@ -134,9 +69,15 @@ def find_consts():
         s = text[i:i + 2]
         up_i()
         up_i()
-        while is_digit(text[i]):
-            s += text[i]
+        if is_digit_16_not_zero(text[i]):
+            s = text[i]
             up_i()
+            while is_digit_16(text[i]):
+                s += text[i]
+                up_i()
+            print('DEC: ' + s)
+        else:
+            print('ERROR')
         if s == '0x':
             print('ERROR')
         else:
@@ -145,28 +86,8 @@ def find_consts():
 
 def find_two_symbols():
     global i, text
-    if text[i:i + 2] == '>>':
-        print('R_SHIFT')
-        up_i()
-        up_i()
-    if text[i:i + 2] == '<<':
-        print('L_SHIFT')
-        up_i()
-        up_i()
-    if text[i:i + 2] == '==':
-        print('EQ')
-        up_i()
-        up_i()
-    if text[i:i + 2] == '!=':
-        print('NOT_EQ')
-        up_i()
-        up_i()
-    if text[i:i + 2] == '<=':
-        print('LESS_EQ')
-        up_i()
-        up_i()
-    if text[i:i + 2] == '>=':
-        print('GREATER_EQ')
+    if text[i:i + 2] in two_symbols.keys():
+        print(two_symbols[text[i:i + 2]])
         up_i()
         up_i()
 
