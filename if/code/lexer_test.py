78d677742d0cc2cf1a5b16914f42bb6dd0834834
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2021-05-12 22:25:52 +0700

    up lexer

--- if/code/lexer_test.py
+++ if/code/lexer_test.py
@@ -12,40 +12,67 @@ def program():
     elif is_type(lex.name):
         variable()
     else:
-        print('Ожидался тип (short, int, long) или void')
+        err('Ожидался тип (short, int, long) или void')
 
 
 def function():
     lex = next_lex()
     if lex.name != 'VOID':
-        print('Ожидался void')
+        err('Ожидался void')
     lex = next_lex()
     if lex.name != 'ID':
-        raise Exception(f'Ожидался идентификатор {lex}')
-    function_name = lex.value
+        err(f'Ожидался идентификатор {lex}')
     lex = next_lex()
     if lex.name != 'ROUND_LEFT':
-        raise Exception(f'Ожидался ( {lex}')
+        err(f'Ожидался ( {lex}')
     lex = next_lex()
     if lex.name != 'ROUND_RIGHT':
-        raise Exception(f'Ожидался ) {lex}')
+        err(f'Ожидался ) {lex}')
     composite_operator()
 
 
 def composite_operator():
     lex = next_lex()
     if lex.name != 'CURLY_LEFT':
-        print('Ожидался {')
-    #  TODO
+        err('Ожидался {')
+    lex = read_lex()
+    if lex.name == 'ID':
+        call_function()
+    lex = next_lex()
     if lex.name != 'CURLY_RIGHT':
-        print('Ожидался }')
+        err('Ожидался }')
+
+
+def call_function():
+    lex = next_lex()
+    if lex.name != 'ID':
+        err('Ожидался идентификатор')
+    lex = next_lex()
+    if lex.name != 'ROUND_LEFT':
+        err('Ожидался (')
+    expression()
+    lex = next_lex()
+    if lex.name != 'ROUND_RIGHT':
+        err('Ожидался )')
+    lex = next_lex()
+    if lex.name != 'SEMICOLON':
+        err('Ожидался ;')
+
+
+def expression():
+    lex = next_lex()
 
 
 def variable():
     lex = next_lex()
     if is_type(lex.name):
-        print('Ожидался тип (short, int, long)')
+        err('Ожидался тип (short, int, long)')
     #  TODO
+
+
+def err(text: str):
+    print(text)
+    exit(1)
 
 
 if __name__ == '__main__':
